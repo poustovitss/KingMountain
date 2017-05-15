671c3afc5644a5968f0d93987dd514d78bbe742e
Pr Ulan <b.a.w@bkru>
2017-05-15 15:40:22 +0600

    proverka

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,18 +7,22 @@ class ProfilesController < ApplicationController
   def update 
  	randombot = rand(1...1000000000)
   # name = "donate_#{current_user.id}_#{Time.now}"
- 
-    if current_user.balance > 50
-      a = current_user  
-      a.refferences.new(name: "Donate#{randombot}", email: "Donate#{randombot}@m.r", password: 'yk,Y@&7v`-8n-C3c', reffered_by: current_user.id)
-      a.balance = a.balance - 50
-      a.save
-
-      redirect_to :back
-    else
+    if current_user.balance.nil?
       flash[:balance] = 'Не достаточно баланса'
-      redirect_to :back
-    end   
+        redirect_to :back
+    else
+      if current_user.balance > 50 && current_user.balance > 0
+        a = current_user  
+        a.refferences.new(name: "Donate#{randombot}", email: "Donate#{randombot}@m.r", password: 'yk,Y@&7v`-8n-C3c', reffered_by: current_user.id)
+        a.balance = a.balance - 50
+        a.save
+
+        redirect_to :back
+      else
+        flash[:balance] = 'Не достаточно баланса'
+        redirect_to :back
+      end   
+    end
   end
   
 end
