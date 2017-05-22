f20e21ed788fee0bb55720a11260cfe5c982a115
Pr Ulan <b.a.w@bkru>
2017-05-22 15:35:43 +0600

    Выпадашка для описания улучшений

--- app/controllers/improves_controller.rb
+++ app/controllers/improves_controller.rb
@@ -41,28 +41,3 @@ class ImprovesController < ApplicationController
   end
 end
 
-      # if current_user.balance >= 25
-      #   unless current_user.reffered.nil?
-      #         reffered = current_user.reffered
-      #         reffered.balance += 25*0.75
-      #         reffered.save
-      #     end
-          
-
-      #     transfer = Transfer.new
-      #     transfer.user_id = current_user.id
-      #     transfer.bank_id = Bank.last.id
-      #     transfer.summa = 25*0.20
-      #     transfer.save            
-
-      #     unless current_user.reffered.nil?
-      #       transfer = Transfer.new
-      #       transfer.user_id = current_user.reffered.id
-      #       transfer.bank_id = Bank.last.id
-      #       transfer.summa = 25*0.05
-      #       transfer.save
-      #     end
-          
-      #     user = current_user
-      #     user.balance = current_user.balance - 25
-      #     user.save
