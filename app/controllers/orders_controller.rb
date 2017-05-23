fcfaae5a373d1bc8bf0d6a2b85d35d336afe3ed5
Pr Ulan <b.a.w@bkru>
2017-05-24 01:56:20 +0600

    debug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,7 +19,12 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:ik_pm_no])
     @user = current_user
 
+    puts @order
+
     if (params[:ik_inv_st] ="success" and (params[:ik_ps_price]||0)==(@order.total||0) )
+      puts '----------------------------------'
+      puts '----------------------------------'
+      puts '----------------------------------'
       @order.user.balance += @order.total
       @order.user.save
       flash[:balance] = "Оплата прошла, сумма #{order.total}"
