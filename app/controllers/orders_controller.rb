0d3ff7d68cf0a0b088de6d7dbaf787b1a70e2eee
Pr Ulan <b.a.w@bkru>
2017-05-24 00:30:19 +0600

    apishka

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,21 +16,29 @@ class OrdersController < ApplicationController
   end
 
   def success
-    @order = Order.last
-    @user = current_user
-
-  
-    if @user.id == @order.user.id
-
-      @order.user.balance += @order.total
-      @order.user.save
-      flash[:balance] = 'Оплата прошла заебись'
-      
-      redirect_to profiles_path
+     @order = order_class.find(params['ik_pm_no'])
+    if not @order
+      logger.debug "Interkassa error: order not found"
+      render text: 'Order not found', status: 500
     else
-      flash[:balance] = 'Оплата не прошла :('
+      flash[:balance] = 'Оплата прошла заебись'
       redirect_to profiles_path
     end
+    # @order = Order.last
+    # @user = current_user
+
+  
+    # if @user.id == @order.user.id
+
+    #   @order.user.balance += @order.total
+    #   @order.user.save
+    #   flash[:balance] = 'Оплата прошла заебись'
+      
+    #   redirect_to profiles_path
+    # else
+    #   flash[:balance] = 'Оплата не прошла :('
+    #   redirect_to profiles_path
+    # end
   end
   
   def update
