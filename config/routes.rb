700bfb6d3d95a059ec08af42ec6d4cf7a4355839
Pr Ulan <b.a.w@bkru>
2017-05-23 23:29:21 +0600

    API2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
  resources :homepages, :profiles, :improves, :orders
 
- post 'success/:id' => 'orders#success' #as: 'success'
+  match '/success' => 'orders#success', via: :get
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
