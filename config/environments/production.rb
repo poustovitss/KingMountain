f2a897845e8de392fe55920d219c2075fe16da61
Pr Ulan <b.a.w@bkru>
2017-05-25 17:46:54 +0600

    production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,12 +87,13 @@ Rails.application.configure do
    #SMTP
   config.action_mailer.default_url_options = { :host => 'king-mountain.pro' }  
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "king-mountain.pro",
   :user_name            => ENV['SENDMAIL_USERNAME'],
   :password             => ENV['SENDMAIL_PASSWORD'],
   :authentication       => 'plain',
+  :openssl_verify_mode  => 'none',
   :enable_starttls_auto => true  }
 end
