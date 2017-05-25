7c2871c05504b51f3a75967a3f817fe0d082158e
Pr Ulan <b.a.w@bkru>
2017-05-25 17:43:56 +0600

    Email Auntificate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
+  :domain               => "king-mountain.pro",
   :user_name            => ENV['SENDMAIL_USERNAME'],
   :password             => ENV['SENDMAIL_PASSWORD'],
   :authentication       => 'plain',
