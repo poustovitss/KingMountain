8718d801282af750cf2b1e41ca96c7711b63e1b4
Pr Ulan <b.a.w@bkru>
2017-05-25 17:50:41 +0600

    production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
    #SMTP
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'king-mountain.pro' }
   ActionMailer::Base.smtp_settings = {
 
