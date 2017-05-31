64c6212b7dd2139e2f7505a6cc51fa355d619822
Pr Ulan <b.a.w@bkru>
2017-05-31 23:14:47 +0600

    Thumb

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 require 'dotenv'
-require 'link_thumbnailer'
 Dotenv.load('onlinepay.env')
 
 module Kingofmountain
