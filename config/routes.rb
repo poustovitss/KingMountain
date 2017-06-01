aa6b2a7c41bae373f6c124cc683525a6b18b2dc2
Pr Ulan <b.a.w@bkru>
2017-06-01 16:40:45 +0600

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { confirmations: 'confirmations', omniauth_callbacks: 'omniauth_callbacks' }
 
-  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch]
+  match '/users/:id' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepages#index'
