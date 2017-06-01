aecfcb8f61acd5ce4253b7ea11b5d5993e2619fc
Pr Ulan <b.a.w@bkru>
2017-06-01 16:47:55 +0600

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,8 @@ class UsersController < ApplicationController
   # GET/PATCH /users/:id/finish_signup
   def finish_signup
     # authorize! :update, @user 
-    if request.patch? && params[:user] #&& params[:user][:email]
-      if @user.update_attribute(user_params)
+    if request.patch? && params[:user][:email] #&& params[:user][:email]
+      if @user.update(user_params)
         @user.skip_reconfirmation!
         sign_in(@user, :bypass => true)
         redirect_to @user, notice: 'Your profile was successfully updated.'
