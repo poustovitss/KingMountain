25e95dc73e7e9a7d55f657061df768d2812f7a49
Pr Ulan <b.a.w@bkru>
2017-06-08 17:31:46 +0600

    Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ TEMP_EMAIL_PREFIX = 'change@me'
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable
   devise :database_authenticatable, :registerable, :confirmable,
-    :recoverable, :rememberable, :trackable, :validatable, :omniauthable
-    
-    # :lastseenable
+    :recoverable, :rememberable, :trackable, :validatable, :lastseenable,
+    :omniauthable
 
   validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
 
