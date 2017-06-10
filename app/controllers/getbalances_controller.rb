50c0641ffce8e140da44e0e467c94f3eebe7e77d
Pr Ulan <b.a.w@bkru>
2017-06-10 19:04:38 +0600

    Fix рабочего баланса

--- app/controllers/getbalances_controller.rb
+++ app/controllers/getbalances_controller.rb
@@ -6,6 +6,7 @@ class GetbalancesController < ApplicationController
   def create
     if current_user.level >= 3  
       if current_user.balance > 150
+     
         @getbalance = Getbalance.new(getbalance_params)
 
         user = current_user
@@ -15,27 +16,26 @@ class GetbalancesController < ApplicationController
           percent = @getbalance.total * 30 / 100
         end
 
-        @getbalance.total = @getbalance.total - percent
-        @getbalance.save
+        if @getbalance.total <= current_user.balance 
+            
+            @getbalance.total = @getbalance.total - percent
+            @getbalance.save
+            
+            user.balance = 0
+            user.save
 
-        user = current_user
-        if current_user.aerodrome == false
-          percent = @getbalance.total * 50 / 100
+            user.level = 0
+            user.refferences.each do |refferences|
+              refferences.reffered_by = 0
+              refferences.save
+            end
+          user.save
+
+          flash[:balance] = 'Ваш запрос на вывод средств скоро будет обработан!'
         else
-          percent = @getbalance.total * 30 / 100
+          flash[:balance] = "У вас не достаточно баланса"
         end
-        
-        user.balance = 0
-        user.save
 
-        user.level = 0
-        user.refferences.each do |refferences|
-          refferences.reffered_by = 0
-          refferences.save
-        end
-        user.save
-
-        flash[:balance] = 'Ваш запрос на вывод средств скоро будет обработан!'
       else
         flash[:balance] = 'Что-бы вывести средства у вас должно быть не менее 150рублей'
       end
