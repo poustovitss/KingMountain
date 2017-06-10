ad9a9bdc66fb6f3daba80b26881e27ea322ae4ff
Pr Ulan <b.a.w@bkru>
2017-06-10 18:35:29 +0600

    Теперь все обнуляется если сливать деньги с рабочего Деньги снимать можно с свободного без ограничений

--- app/controllers/getmoneys_controller.rb
+++ app/controllers/getmoneys_controller.rb
@@ -4,8 +4,6 @@ class GetmoneysController < ApplicationController
   end
 
   def create
-    if current_user.level >= 3
-      if current_user.transfers.sum(:summa) > 100
         @getmoney = Getmoney.new(getmoney_params)
         @getmoney.save
 
@@ -13,12 +11,6 @@ class GetmoneysController < ApplicationController
         user.summa = user.summa - @getmoney.total
         user.save
         flash[:balance] = 'Ваш запрос на вывод средств скоро будет обработан!'
-      else
-        flash[:balance] = 'Что-бы вывести средства у вас должно быть не менее 100рублей'
-      end
-    else
-      flash[:balance] = 'Вам требуется 3 уровень'
-    end   
     redirect_to profiles_path
   end
 
