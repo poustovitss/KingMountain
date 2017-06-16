16cae8a2cc41e7ac8eff671eee8aaf908a320c2f
Pr Ulan <b.a.w@bkru>
2017-06-16 16:35:54 +0600

    Vkontakte

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,14 @@ TEMP_EMAIL_PREFIX = 'change@me'
   #   Devise::Mailer.send_new_user_message(self).deliver  
   # end
 
+  def self.find_for_vkontakte_oauth access_token
+    if user = User.where(:url => access_token.info.urls.Vkontakte).first
+      user
+    else 
+      User.create!(:provider => access_token.provider, :url => access_token.info.urls.Vkontakte, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :email => access_token.extra.raw_info.domain+'@vk.com', :password => Devise.friendly_token[0,20]) 
+    end
+  end
+
   def self.find_for_oauth(auth, signed_in_resource = nil)
 
     # Get the identity and user if they exist
