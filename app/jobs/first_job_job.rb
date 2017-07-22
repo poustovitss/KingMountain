e274c7ac1aebd91b6a946173bbe6ec29ac8215f7
Pr Ulan <b.a.w@bkru>
2017-07-22 22:08:27 +0600

    Active Job

--- app/jobs/first_job_job.rb
+++ app/jobs/first_job_job.rb
@@ -4,7 +4,7 @@ class FirstJobJob < ApplicationJob
   def perform(user)
   	# user = current_user
 
-    if user.reffered_by == 0 && user.level > 1
+  if user.reffered_by == 0 && user.level > 1
 	  system_users = User.where(level: user.level + 1)
 
 	  system_user = system_users.first
