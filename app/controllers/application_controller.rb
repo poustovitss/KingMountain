6999982bfdead0b983d192f878d3b1ba615a0004
altynbek1977 <altiwbek@gmail.com>
2017-07-28 15:46:16 +0600

    Добавлен счетчик длительности игры - улучшен счетчик онлайн пользователей

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 	before_filter :capture_referal, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
-  after_filter :set_online
+  before_filter :set_online
+
+  helper_method :all_who_are_in_touch
   
   def ensure_signup_complete
     # Убеждаемся, что цикл не бесконечный
@@ -16,16 +18,29 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # возвращает кол-во онлайн юзеров
+  # если 'redis' отключен/не работает, возвращает 15. (с точкой, это типа намек)
   def all_who_are_in_touch
-    $redis_onlines.keys.count
+    begin
+      $redis_onlines.keys.count
+    rescue Redis::CannotConnectError
+      return '15.'
+    end
   end
 
   private
 
+  # отправляет 'id' юзера в редис как залогиненного
+  # если в течении 10 минут не обновил страничку то удаляется с базы 'redis'
   def set_online
-    if !!current_user
-      $redis_onlines.set( current_user.id, nil, ex: 10*60 )
+    begin
+      if !!current_user
+        $redis_onlines.set( current_user.id, nil, ex: 10*60 ) #здесь нужно 10 изменить если хотите другой интервал
+      end
+    rescue Redis::CannotConnectError => e
+      # p e.message
     end
+
   end
 
   def set_locale
