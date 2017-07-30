ec5efd4776dae220b923072b8200fef1b5ea5daa
Pr Ulan <b.a.w@bkru>
2017-07-30 19:09:15 +0600

    Debug js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+//= require bootstrap
 
 // $(document).ready(function() {
 // document.getElementById("copyButton").addEventListener("click", function() {
