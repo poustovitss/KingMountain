86d33c68b0162fa3562dea19e7d8638f04ce3608
Pr Ulan <b.a.w@bkru>
2017-07-30 18:12:18 +0600

    Debug code

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 require 'dotenv'
-Dotenv.load('/onlinepay.env')
+Dotenv.load('onlinepay.env')
 
 module Kingofmountain
   class Application < Rails::Application
