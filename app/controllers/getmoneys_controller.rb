759419797cd3c4cd3800ef93eaba5ea234e9ac03
Pr Ulan <b.a.w@bkru>
2017-07-30 17:38:04 +0600

    Поставил проверку на вывод денег

--- app/controllers/getmoneys_controller.rb
+++ app/controllers/getmoneys_controller.rb
@@ -4,13 +4,23 @@ class GetmoneysController < ApplicationController
   end
 
   def create
-        @getmoney = Getmoney.new(getmoney_params)
+    vivod = Transfer.find_by_user_id(current_user.id)
+    @getmoney = Getmoney.new(getmoney_params)
+    
+    if @getmoney.total >= 50
+      if vivod.summa >= @getmoney.total
         @getmoney.save
 
         user = Transfer.find_by_user_id(current_user.id)
         user.summa = user.summa - @getmoney.total
         user.save
         flash[:balance] = 'Ваш запрос на вывод средств скоро будет обработан!'
+      else
+        flash[:balance] = 'У вас не достаточно средств'
+      end
+    else
+      flash[:balance] = 'Минимальная сумма вывода 50рублей'
+    end
     redirect_to profiles_path
   end
 
