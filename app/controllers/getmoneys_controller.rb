fc24b99057d1274c2d14b9ffa02fc872e00fd66e
Pr Ulan <b.a.w@bkru>
2017-07-30 19:36:40 +0600

    Исправил баг с выводом

--- app/controllers/getmoneys_controller.rb
+++ app/controllers/getmoneys_controller.rb
@@ -7,19 +7,23 @@ class GetmoneysController < ApplicationController
     vivod = Transfer.find_by_user_id(current_user.id)
     @getmoney = Getmoney.new(getmoney_params)
     
-    if @getmoney.total >= 50
-      if vivod.summa >= @getmoney.total
-        @getmoney.save
+    unless vivod.nil?
+      if @getmoney.total >= 50
+        if vivod.summa >= @getmoney.total
+          @getmoney.save
 
-        user = Transfer.find_by_user_id(current_user.id)
-        user.summa = user.summa - @getmoney.total
-        user.save
-        flash[:balance] = 'Ваш запрос на вывод средств скоро будет обработан!'
+          user = Transfer.find_by_user_id(current_user.id)
+          user.summa = user.summa - @getmoney.total
+          user.save
+          flash[:balance] = 'Ваш запрос на вывод средств скоро будет обработан!'
+        else
+          flash[:balance] = 'У вас не достаточно средств'
+        end
       else
-        flash[:balance] = 'У вас не достаточно средств'
+        flash[:balance] = 'Минимальная сумма вывода 50рублей'
       end
     else
-      flash[:balance] = 'Минимальная сумма вывода 50рублей'
+      flash[:balance] = 'У вас не достаточно средств'
     end
     redirect_to profiles_path
   end
